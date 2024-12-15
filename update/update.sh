9419c794e7b14cb46973a0225576716003286271
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2024-12-15 19:45:23 -0400

    Add scripts to update files more easily, Add `metube_ajspec` argument

---
+++ update/update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+. "${BASEDIR}/update.conf"
+
+set -xe
+set -o pipefail
+
+cat -- "${BASEDIR}/Makejail.template" |\
+    sed -Ee "s/%%TAG1%%/${TAG1}/g" > "${BASEDIR}/../Makejail"
+
+cat -- "${BASEDIR}/README.md.template" |\
+    sed -E \
+        -e "s/%%TAG1%%/${TAG1}/g" \
+        -e "s/%%TAG2%%/${TAG2}/g" > "${BASEDIR}/../README.md"
